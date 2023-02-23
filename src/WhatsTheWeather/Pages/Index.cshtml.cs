57c613ba16f419eadf88bd49e0ffaedc576b6532
Samuel Peter <sapeter@ethz.ch>
2023-02-23 13:27:24 +0100

    add target race duration and derive checkpoint time

--- src/WhatsTheWeather/Pages/Index.cshtml.cs
+++ src/WhatsTheWeather/Pages/Index.cshtml.cs
@@ -10,10 +10,11 @@ public class IndexModel : PageModel
     private readonly IRepository<int, Race> _raceRepo;
     private readonly HttpClient _api;
 
-    public string? RaceName { get; private set; }
-    public int? RaceYear { get; private set; }
-    public DateTime? Start { get; private set; }
-    public Dictionary<string, (int Km, Weather Data)> Forecast { get; private set; } = new();
+    public string RaceName { get; private set; }
+    public int RaceYear { get; private set; }
+    public TimeSpan RaceDuration { get; private set; }
+    public Race Race { get; private set; }
+    public Dictionary<string, (DateTime Time, int Km, Weather Data)> Forecast { get; private set; } = new();
 
     public IndexModel(
         ILogger<IndexModel> logger,
@@ -27,36 +28,66 @@ public class IndexModel : PageModel
             BaseAddress = new Uri(uri)
         };
         _logger.LogInformation($"api for data: {_api.BaseAddress.AbsoluteUri}");
-    }
-
-    public async Task OnGetAsync()
-    {
         // to be set from UI later
         RaceName = "Vasaloppet";
         RaceYear = 2023;
+        RaceDuration = TimeSpan.FromHours(6);
+        Race = GetRaceAsync(RaceName, RaceYear).Result;
+        // init the forecast dict and fill with meteo data
+        var totalDistance = Race.Checkpoints.Last().Distance;
+        foreach (var checkpoint in Race.Checkpoints)
+        {
+            var record = GetRecordAsync(checkpoint.Location, Race.Start, false).Result;
+            var time = Race.Start + RaceDuration * (double)checkpoint.Distance/totalDistance;
+            Forecast.Add(checkpoint.Name, (time, checkpoint.Distance, record.Data));
+        }
+    }
+
+    public void OnGet()
+    {
+        // for now get meteo data again (in case it was updated in internal api)
+        foreach (var checkpoint in Race.Checkpoints)
+        {
+            var record = GetRecordAsync(checkpoint.Location, Race.Start, false).Result;
+            UpdateForecast(checkpoint.Name, record.Data);
+        }
+    }
+
+    private async Task<Race> GetRaceAsync(string name, int year)
+    {
         // get the race
-        var response = await _api.GetAsync($"races/{RaceName}/{RaceYear}");
+        var response = await _api.GetAsync($"races/{name}/{year}");
         response.EnsureSuccessStatusCode();
         var race = Race.FromJson(await response.Content.ReadAsStringAsync());
         if (race == null)
         {
-            _logger.LogError($"Race {RaceName} in year {RaceYear} not found.");
-            return;
+            var msg = $"Race {name} in year {year} not found.";
+            _logger.LogError(msg);
+            throw new KeyNotFoundException(msg);
         }
-        Start = race.Start;
-        // get the meteo forecast for all checkpoints
-        foreach (var checkpoint in race.Checkpoints)
+        return race;
+    }
+
+    private async Task<WeatherRecord> GetRecordAsync(Coordinates location, DateTime time, bool update)
+    {
+        var request = new WeatherRequest(location, time);
+        var response = await _api.PostAsJsonAsync<WeatherRequest>($"weather?ForceExternal={update}", request);
+        response.EnsureSuccessStatusCode();
+        var record = WeatherRecord.FromJson(await response.Content.ReadAsStringAsync());
+        if (record == null)
         {
-            var request = new WeatherRequest(checkpoint.Location, race.Start);
-            response = await _api.PostAsJsonAsync<WeatherRequest>("weather", request);
-            response.EnsureSuccessStatusCode();
-            var record = WeatherRecord.FromJson(await response.Content.ReadAsStringAsync());
-            if (record == null)
-            {
-                _logger.LogError($"No forecast found for {request}.");
-                return;
-            }
-            Forecast.Add(checkpoint.Name, (checkpoint.Distance, record.Data));
+            var msg = $"No forecast found for {request}.";
+            _logger.LogError(msg);
+            throw new KeyNotFoundException(msg);
+        }
+        return record;
+    }
+
+    private void UpdateForecast(string name, Weather data)
+    {
+        if (Forecast.TryGetValue(name, out var forecast))
+        {
+            forecast.Data = data;
         }
     }
 }
